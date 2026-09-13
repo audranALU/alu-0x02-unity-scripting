a9ad42bc9796bfa1c06ecf04202a68d87e5a5a17
audranALU <a.ndayiseng@gmail.com>
2026-09-14 00:06:47 +0200

    creating more coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
 
     private Rigidbody rb;
+    private int score = 0;
 
     void Start()
     {
@@ -19,5 +20,15 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(45 * Time.deltaTime, 0, 0);
+    }
+}
